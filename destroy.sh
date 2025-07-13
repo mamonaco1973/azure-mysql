15794b760a4187b388dedf3204b86021b38f3815
Mike Monaco <mamonaco1973@gmail.com>
2025-07-13 17:35:08 -0400

    mysql conversion

--- destroy.sh
+++ destroy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------------------
-# STEP 1: Destroy postgres infrastructure (VNet, Subnet, NICs, NSGs, etc.)
+# STEP 1: Destroy mysql infrastructure (VNet, Subnet, NICs, NSGs, etc.)
 #-------------------------------------------------------------------------------
-cd 01-postgres                     # Go to base infra config
-terraform init                     # Initialize Terraform plugins/modules
-terraform destroy -auto-approve    # Destroy all foundational Azure resources
-cd ..                              # Return to root
+cd 01-mysql                            # Go to base infra config
+terraform init                         # Initialize Terraform plugins/modules
+terraform destroy -auto-approve        # Destroy all foundational Azure resources
+cd ..                                  # Return to root
 
 #-------------------------------------------------------------------------------
 # END OF SCRIPT
--- validate.sh
+++ validate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#-------------------------------------------------------------------------------
-# Output pgweb URL and postgres DNS name
-#-------------------------------------------------------------------------------
+# #-------------------------------------------------------------------------------
+# # Output pgweb URL and postgres DNS name
+# #-------------------------------------------------------------------------------
 
-PGWEB_DNS_NAME=$(az network public-ip show \
-  --name pgweb-vm-public-ip \
-  --resource-group postgres-rg \
-  --query "dnsSettings.fqdn" \
-  --output tsv)
+# PGWEB_DNS_NAME=$(az network public-ip show \
+#   --name pgweb-vm-public-ip \
+#   --resource-group postgres-rg \
+#   --query "dnsSettings.fqdn" \
+#   --output tsv)
 
-echo "NOTE: pgweb running at http://$PGWEB_DNS_NAME"
+# echo "NOTE: pgweb running at http://$PGWEB_DNS_NAME"
 
-PG_DNS=$(az postgres flexible-server list \
-  --resource-group postgres-rg \
-  --query "[?starts_with(name, 'postgres-instance')].fullyQualifiedDomainName" \
-  --output tsv)
+# PG_DNS=$(az postgres flexible-server list \
+#   --resource-group postgres-rg \
+#   --query "[?starts_with(name, 'postgres-instance')].fullyQualifiedDomainName" \
+#   --output tsv)
 
-echo "NOTE: Hostname for postgres server is \"$PG_DNS\""
+# echo "NOTE: Hostname for postgres server is \"$PG_DNS\""
 
 #-------------------------------------------------------------------------------
 # END OF SCRIPT
